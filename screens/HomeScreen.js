77e3faa6711fd0868a69f917ada7af8a7a005fee
Arthur <arthur_2335@hotmail.com>
2022-07-22 21:54:39 -0400

    added image to homescreen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,22 @@
 import React from "react";
-import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import { StyleSheet, Text, View, SafeAreaView, Image } from "react-native";
 import tw from "tailwind-react-native-classnames";
 
 const HomeScreen = () => {
   return (
     <SafeAreaView style={tw`bg-white h-full`}>
-      <Text style={tw`text-red-500 p-10`}>I am the HomeScreen</Text>
+      <View>
+        <Image
+          style={{
+            width: 100,
+            height: 100,
+            resizeMode: "contain",
+          }}
+          source={{
+            uri: "https://links.papareact.com/gzs",
+          }}
+        />
+      </View>
     </SafeAreaView>
   );
 };
