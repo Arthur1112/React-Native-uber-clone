ba0221f285300f5f0d04218a93ca63f365f7e919
Arthur <arthur_2335@hotmail.com>
2022-07-22 21:51:07 -0400

    added react native TW

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import tw from "tailwind-react-native-classnames";
 
 const HomeScreen = () => {
   return (
-    <SafeAreaView>
-      <Text>I am the HomeScreen</Text>
+    <SafeAreaView style={tw`bg-white h-full`}>
+      <Text style={tw`text-red-500 p-10`}>I am the HomeScreen</Text>
     </SafeAreaView>
   );
 };
