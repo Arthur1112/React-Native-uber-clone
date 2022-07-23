52e74726540cdfd65948d39ba4d6958a509ca8cb
Arthur <arthur_2335@hotmail.com>
2022-07-22 21:40:12 -0400

    added store and navSlices

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { Provider } from "react-redux";
+import { store } from "./store";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Lets Build Uber!</Text>
-    </View>
+    <Provider store={store}>
+      <View style={styles.container}>
+        <Text>Lets Build Uber!</Text>
+      </View>
+    </Provider>
   );
 }
 
